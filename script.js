0723293e7d9600839569e327a9762465a090e8b3
Oayeni1 <Oayeni88@gmail.com>
2023-05-12 15:27:20 +0100

    still commit

--- script.js
+++ script.js
@@ -25,12 +25,12 @@ const doc1 = document.getElementById('flexSwitchCheckChecked');
 const doc2 = document.querySelector('body');
 doc1.addEventListener('change', function(){
     if (doc1.checked){
-        doc2.classList.add('active')
+        doc2.classList.add('active');
     } 
     else {
-        doc2.classList.remove('active')
+        doc2.classList.remove('active');
     }
-});
+})
 
 
 async function getAPI(){
